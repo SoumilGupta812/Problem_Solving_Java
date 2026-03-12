2bee67ee09e19a956628954aff44e966e9af8723
blac-coffee <soumilgupta.812@gmail.com>
2026-03-12 20:28:44 +0530

    dp problems

--- Dp/fibo.java
+++ Dp/fibo.java
@@ -78,4 +78,14 @@ public class fibo {
         }
         return dp[amount] > amount ? -1 : dp[amount];
     }
+    public int change(int amount, int[] coins) {
+        int[] dp=new int[amount+1];
+        dp[0]=1;
+        for(int coin:coins){
+            for(int i=coin;i<=amount;i++){
+                dp[i]=dp[i]+dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
 }
