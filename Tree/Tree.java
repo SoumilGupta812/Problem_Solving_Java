94940ee24f1a6692e183f1ab159e1269782c67d0
blac-coffee <soumilgupta.812@gmail.com>
2026-03-20 23:30:57 +0530

    did tree serialization and deserialization

--- Tree/Tree.java
+++ Tree/Tree.java
@@ -1,5 +1,5 @@
 package Tree;
-
+import java.util.*;
 public class Tree {
     class TreeNode {
       int val;
@@ -26,5 +26,40 @@ public class Tree {
         int right=Math.max(0,maxPath(root.right));
         max=Math.max(max,left+right+root.val);
         return root.val+Math.max(left,right);
+    }
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder ser = new StringBuilder();
+        serializeHelper(root, ser);
+        return ser.toString();
+    }
+
+    private void serializeHelper(TreeNode root, StringBuilder ser) {
+        if (root == null) {
+            ser.append("#,");
+            return;
+        }
+        ser.append(root.val);
+        ser.append(",");
+        serializeHelper(root.left, ser);
+        serializeHelper(root.right, ser);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] d = data.split(",");
+        Queue<String> q = new LinkedList<>(Arrays.asList(d));
+        return deserializeHelper(data, q);
+    }
+
+    private TreeNode deserializeHelper(String data, Queue<String> q) {
+        String curr = q.poll();
+        if (curr.equals("#"))
+            return null;
+        TreeNode head = new TreeNode();
+        head.val = Integer.parseInt(curr);
+        head.left = deserializeHelper(data, q);
+        head.right = deserializeHelper(data, q);
+        return head;
     }
 }
